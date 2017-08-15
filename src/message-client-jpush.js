a4bbe8269e9ae2e53a69e8e0980520b7d04a4e55
firegnu <firegnu@gmail.com>
2017-08-15 08:50:41 +0800

    comportable for ios
    comportable for ios

--- src/message-client-jpush.js
+++ src/message-client-jpush.js
@@ -5,6 +5,8 @@
 import JPushModule from 'jpush-react-native';
 import client from 'p2m-message-client';
 
+var { NativeAppEventEmitter, Platform } = require('react-native');
+
 const channelId = 'jpush';
 let _options;
 
@@ -15,7 +17,7 @@ function channel(options) {
 
   function on(event, callback) {
     let list = events[event] = events[event] || [];
-    list.push({cb: callback});
+    list.push({ cb: callback });
 
     if (event === 'connect') {
       if (isConnected) {
@@ -57,37 +59,46 @@ function channel(options) {
     _options = Object.assign({}, options, opt2);
 
     console.log("[JPUSH] JPUSH client is starting...");
-
-    JPushModule.addReceiveNotificationListener(onMessage.bind(this));
-    JPushModule.addReceiveOpenNotificationListener(onOpenMessage.bind(this));
+    if(Platform.OS === 'ios') {
+      NativeAppEventEmitter.addListener('ReceiveNotification',(notification) => onMessage(notification));
+      NativeAppEventEmitter.addListener('OpenNotification',(notification) => onOpenMessage(notification));
+    } else {
+      JPushModule.addReceiveNotificationListener(onMessage.bind(this));
+      JPushModule.addReceiveOpenNotificationListener(onOpenMessage.bind(this));
+    }
 
     JPushModule.getRegistrationID(function (deviceId) {
-      client.register(_options.userId, deviceId, channelId)
-          .then(function () {
-            emit('connect', self);
-          });
+      client.register(_options.userId, deviceId, channelId).then(function () {
+        emit('connect', self);
+      });
     });
   }
   function stop() {
     console.log(`[JPUSH] Stopping service...`);
-    JPushModule.removeReceiveNotificationListener(onMessage)
+    JPushModule.removeReceiveNotificationListener(onMessage);
     emit('disconnect', this);
   }
 
   function onMessage(map) {
     console.log(`[JPUSH] Got a message`);
     let fullPath = _options.serverUrl + _options.path;
-    let message = JSON.parse(map.extras);
-    let notificationId = map.notificationId;
+    let message;
+    if(Platform.OS === 'ios') {
+      message = map;
+      //console.log(`[JPUSH] Set message as delivered success.`);
+    } else {
+      message = JSON.parse(map.extras);
+    }
+    //let notificationId = map.notificationId;
     fetch(`${fullPath}/delivered`, {
       method: 'post',
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify({pushId: message.pushId}),
-      credentials: 'include',
-    }).then((res) => {
+      body: JSON.stringify({ pushId: message.pushId }),
+      credentials: 'include'
+    }).then(res => {
       if (!res.ok) {
         let err = `Set message as delivered failed with error state ${res.state}`;
         console.error(`[JPUSH] ${err}`);
@@ -95,7 +106,7 @@ function channel(options) {
       }
 
       return res.json();
-    }).then((result) => {
+    }).then(result => {
       if (result.success) {
         console.log(`[JPUSH] Set message as delivered success.`);
       } else {
@@ -104,15 +115,18 @@ function channel(options) {
         throw err;
       }
     });
-
     emit('message', message, this);
   }
   function onOpenMessage(map) {
     console.log(`[JPUSH] User press the message on notification panel.`);
-    let message = JSON.parse(map.extras);
+    let message;
+    if(Platform.OS === 'ios') {
+      message = map;
+    } else {
+      message = JSON.parse(map.extras);
+    }
     delete rememberMessages[message.sendId];
     client.read(message.sendId);
-
     emit('openMessage', message, this);
   }
 
@@ -123,7 +137,7 @@ function channel(options) {
     }
   }
 
-  return {start, stop, on, off, forgetMessage, channelId};
+  return { start, stop, on, off, forgetMessage, channelId };
 }
 
 module.exports = channel;
